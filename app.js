db43fd54228d60fd4d0c770fa8acfa78159fb65e
Fahimullah-67 <fahimullah446@gmail.com>
2026-02-25 23:09:36 +0500

    enable CORS origin path

--- app.js
+++ app.js
@@ -8,9 +8,17 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json())
-app.use(cookieParser())
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(cookieParser());
+
+
+
 
 app.get("/", (req, res) => {
   res.send("HOME PAGE");
