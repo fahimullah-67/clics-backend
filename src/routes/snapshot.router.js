af877bd9d5fc82c5dce28db4e3648d72e55ce120
Fahimullah-67 <fahimullah446@gmail.com>
2026-03-10 16:29:59 +0500

    Create Snapshot router

---
+++ src/routes/snapshot.router.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { createSnapshot, deleteSnapshot, getSnapshotsByLoanScheme, viewSnapshot } from "../controllers/snapshot.controller";
+
+const router = Router();
+
+router.route("/snapshots/create").post(createSnapshot);
+router.route("/snapshots/getByLoanScheme").get(getSnapshotsByLoanScheme);
+router.route("/snapshots/View").get(viewSnapshot);
+router.route("/snapshots/delete").put(deleteSnapshot);
+
+export { router as snapshotRouter };
