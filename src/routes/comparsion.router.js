135df988adb3ea639fb4946b55bea42f1a387a1c
Fahimullah-67 <fahimullah446@gmail.com>
2026-03-10 16:29:19 +0500

    create comparsion router

---
+++ src/routes/comparsion.router.js
@@ -0,0 +1,9 @@
+import { compareLoanSchemes, deleteComparison } from "../controllers/comparison.controller";
+import Router from "express";
+
+const router = Router();
+
+router.route("/comparison").post(compareLoanSchemes);
+router.route("/comparison/Delete").delete(deleteComparison);
+
+export  {router as comparisonRouter};
