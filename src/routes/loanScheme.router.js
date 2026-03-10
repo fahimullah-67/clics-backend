0aeda74b9dbabce5db9561fb177d3738d23656af
Fahimullah-67 <fahimullah446@gmail.com>
2026-03-10 16:29:44 +0500

    update loan Scheme router

--- src/routes/loanScheme.router.js
+++ src/routes/loanScheme.router.js
@@ -1,10 +1,16 @@
 import { verifyLoanScheme } from "../controllers/loanScheme.Controller";
 import { verifyToken, verifyTokenAndAdmin } from "../middlewares/verifyToken.middleware";
 
+// router.route("/user/register").post(registerUser);
 
-router.put(
-  "/loan/verify/:id",
-  verifyToken,  // middleware runs first
-  verifyTokenAndAdmin,
-  verifyLoanScheme      // controller runs after
-);
+router.route("/loanSchemes/create").post(verifyToken, verifyTokenAndAdmin, createLoanScheme);
+router.route("/loanSchemes/getAll").get(getAllLoanSchemes);
+router.route("/loanSchemes/getOne/:id").get(getLoanSchemeById);
+router.route("/loanSchemes/update/:id").put(verifyToken, verifyTokenAndAdmin, updateLoanScheme);
+router.route("/loanSchemes/delete/:id").delete(verifyToken, verifyTokenAndAdmin, deleteLoanScheme);
+router.route("/loanSchemes/verify").post(verifyLoanScheme);
+router.route("/filter-loan-schemes").get(filterLoanSchemes);
+router.route("/compare-loan-schemes").post(compareLoanSchemes);
+router.route("/recommend-loan-schemes").post(recommendLoanSchemes);
+
+export { router as loanSchemeRouter };
