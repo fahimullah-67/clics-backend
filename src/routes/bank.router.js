66fdd9364c81ab932733cdfecd86afebb4bf05fd
Fahimullah-67 <fahimullah446@gmail.com>
2026-03-10 16:57:41 +0500

    Debug bank router

--- src/routes/bank.router.js
+++ src/routes/bank.router.js
@@ -12,8 +12,10 @@ const router = Router();
 
 router.route("/banks/create").post(verifyToken, verifyTokenAndAdmin, createBank);
 router.route("/banks/getAll").get(getAllBankData);
-router.route("/banks/getOne/:id").get(getBankById);
-router.route("/banks/update/:id").put(verifyToken, verifyTokenAndAdmin, updateBank);
-router.route("/banks/delete/:id").delete(verifyToken, verifyTokenAndAdmin, deleteBank);
+router.route("/banks/getOne").get(getBankById);
+router.route("/banks/update").put(verifyToken, verifyTokenAndAdmin, updateBank);
+router
+  .route("/banks/delete")
+  .delete(verifyToken, verifyTokenAndAdmin, deleteBank);
 
 export { router as bankRouter };
