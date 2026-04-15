fd29d1d1d863d0cddac00275fdd42c3f8206b0f9
Fahimullah-67 <179002904+fahimullah-67@users.noreply.github.com.>
2026-04-16 01:27:17 +0500

    debug

--- src/routes/loanScheme.router.js
+++ src/routes/loanScheme.router.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+  verifyToken,
   verifyTokenAndAdmin,
   verifyTokenAndAuthorization,
 } from "../middlewares/verifyToken.middleware.js";
@@ -15,19 +16,23 @@ import {
   updateLoanScheme,
   verifyLoanScheme,
 } from "../controllers/loanScheme.Controller.js";
+import { createComparison } from "../controllers/comparison.controller.js";
 
 const router = Router();
 
-router.route("/create").post(verifyTokenAndAdmin, createLoanScheme);
+router.route("/create").post(verifyToken, createLoanScheme);
 router.route("/getAll").get(getAllLoanSchemes);
 router.route("/getOne").get(getLoanSchemeById);
-router.route("/update").put(verifyTokenAndAuthorization, updateLoanScheme);
 router
-  .route("/delete/:id")
+  .route("loanSchemes/update")
+  .put(verifyTokenAndAuthorization, updateLoanScheme);
+router
+  .route("loanSchemes/delete/:id")
   .delete(verifyTokenAndAuthorization, deleteLoanScheme);
-router.route("/verify").post(verifyLoanScheme);
-router.route("/filter-loan-schemes").get(filterLoanSchemes);
-router.route("/compare-loan-schemes").post(compareLoanSchemes);
-router.route("/recommend-loan-schemes").post(recommendLoanScheme);
+router.route("loanSchemes/verify").post(verifyLoanScheme);
+router.route("loanSchemes/filter-loan-schemes").get(filterLoanSchemes);
+router.route("loanSchemes/compare-loan-schemes").post(compareLoanSchemes);
+router.route("loanSchemes/recommend-loan-schemes").post(recommendLoanScheme);
+
 
 export { router as loanSchemeRouter };
