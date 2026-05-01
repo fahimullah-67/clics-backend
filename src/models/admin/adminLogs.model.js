f58d573ea67951962b905270abdef3d8bc41e21d
Fahimullah-67 <179002904+fahimullah-67@users.noreply.github.com.>
2026-05-02 00:43:22 +0500

    fix: correct schema definitions and improve formatting in adminLogs model

--- src/models/admin/adminLogs.model.js
+++ src/models/admin/adminLogs.model.js
@@ -1,39 +1,55 @@
 // AdminLog stores a complete history of admin actions 
 //  so any data change can be tracked and audited later.
-
 import mongoose from "mongoose";
-const adminLogsSchema = new mongoose.Schema({
-    adminId:{
-        type: mongoose.schema.Types.ObjectID,
-        ref: "User",
-        required: true,
+
+
+
+const adminLogsSchema = new mongoose.Schema(
+  {
+    adminId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    action:{
-        type: String,
-        required: true,
-        enum: ["CREATE", "UPDATE", "DELETE", "LOGIN", "LOGOUT", "VERIFY", "APPROVE"],
+    action: {
+      type: String,
+      enum: [
+        "CREATE",
+        "UPDATE",
+        "DELETE",
+        "LOGIN",
+        "LOGOUT",
+        "VERIFY",
+        "APPROVE",
+      ],
+      required: true,
     },
-    targetCollection:{
-        type: String,
-        required: true,
-        enum: ["User", "ChatSession", "LoanScheme", "Comparison", "Banks", "SourceSnapshot"],
+    targetCollection: {
+      type: String,
+      enum: [
+        "User",
+        "ChatSession",
+        "LoanScheme",
+        "Comparison",
+        "Bank",
+        "Snapshot",
+      ],
+      required: true,
     },
-    targetId:{ //Which exact record was changed
-        type: mongoose.schema.Types.ObjectID,
-        required: true,
+    targetId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
     },
-    previousValue:{
-        type: mongoose.Schema.Types.Mixed,
-        required: false,
+    previousValue: {
+      type: mongoose.Schema.Types.Mixed,
     },
-    newValue:{
-        type: mongoose.Schema.Types.Mixed,
-        required: false,
+    newValue: {
+      type: mongoose.Schema.Types.Mixed,
     },
-},
-{
-    timestamps: true,
-})
+  },
+  { timestamps: true },
+);
 
 const AdminLog = mongoose.model("AdminLog", adminLogsSchema);
+
 export default AdminLog;
