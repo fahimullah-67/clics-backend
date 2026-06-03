713f114dac6e846d034ce33791a7ac6b7cf19976
Fahimullah-67 <179002904+fahimullah-67@users.noreply.github.com.>
2026-06-03 16:37:59 +0500

    update code

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,11 @@ import { app } from "../app.js";
 import connectDB from "../src/db/index.js";
 
 export default async function handler(req, res) {
-  await connectDB();
-  return app(req, res);
+  try {
+    await connectDB();
+    return app(req, res);
+  } catch (error) {
+    console.error("Server Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
